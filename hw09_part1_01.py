23509ae1f12ac4a722fcf9788d7adaf2500ac350
JanKlapetek <jan.klapetek@gmail.com>
2024-04-05 17:22:37 +0200

    upraveno velke pismeno

--- hw09_part1_01.py
+++ hw09_part1_01.py
@@ -1,7 +1,7 @@
 #Implementujte třídu Car. V polích třídy by měly být uloženy následující údaje: model, rok vydání, výrobce, objem motoru, barva, cena.
 #Implementujte metody třídy pro vstup a výstup dat, poskytněte přístup k jednotlivým polím prostřednictvím metod třídy.
 
-class car:
+class Car:
     def __init__(self, model, rok, vyrobce, objem, barva, cena):
         self.model = model
         self.rok = rok
@@ -19,8 +19,8 @@ class car:
         print(f'Barva: {self.barva}')
         print(f'Cena: {self.cena}')
 
-moje_auto = car(model='Octavia 2 RS', rok='2013', vyrobce='Škoda', objem=2, barva='Bílá', cena=190000)
-vase_auto = car(model=input('Zadej model auta: '), rok=input('Zadej rok auta: '), vyrobce=input('Zadej výrobce auta: '),
+moje_auto = Car(model='Octavia 2 RS', rok='2013', vyrobce='Škoda', objem=2, barva='Bílá', cena=190000)
+vase_auto = Car(model=input('Zadej model auta: '), rok=input('Zadej rok auta: '), vyrobce=input('Zadej výrobce auta: '),
                 objem=input('Zadej objem auta: '), barva=input('Zadej barvu auta: '), cena=input('Zadej cenu auta: '))
 print('\nMoje auto:')
 moje_auto.info()
