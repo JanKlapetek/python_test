0eb36337e1485e91d4155f6e2afa255c11e37902
JanKlapetek <jan.klapetek@gmail.com>
2024-04-05 17:14:46 +0200

    Device save_01

---
+++ hw_09_part4_01.py
@@ -0,0 +1,5 @@
+# Vytvořte třídu Device obsahující informace o zařízení.
+# Pomocí dědičnosti implementujte třídu CoffeeMachine (obsahuje informace o kávovaru),
+# třídu Blender (obsahuje informace o mixéru), třídu MeatGrinder (obsahuje informace o mlýnku na maso).
+# Každá třída musí mít požadované metody.
+
