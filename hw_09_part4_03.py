0af6fe7ef9ecbc69e4561d78a906eff020400582
JanKlapetek <jan.klapetek@gmail.com>
2024-04-05 19:33:28 +0200

    0.1 update hw_09_part4_03.py

---
+++ hw_09_part4_03.py
@@ -0,0 +1,7 @@
+# Úkol 3
+# Vytvořte třídu Money pro práci s penězi (objekt třídy pracuje s jednou měnou).
+# Třída musí obsahovat pole pro uložení celočíselné části (dolary, eura, hřivny atd.) a pole pro uložení zlomkové části (centy, eurocenty, kopějky atd.).
+# Implementujte metody pro tisk částek a nastavení hodnot částí (celočíselných a zlomkových).
+# Na základě třídy Money vytvořte třídu Product. Implemen- tujte metodu, která umožní snížit cenu o zadané číslo.
+# Implementujte metody a pole požadované pro každou třídu.
+
