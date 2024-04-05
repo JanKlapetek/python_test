0f0d6d2bae933b9fa2cb86ac297f3b1ca2c3ae57
JanKlapetek <jan.klapetek@gmail.com>
2024-04-05 17:28:10 +0200

    0.2 save

--- hw_09_part4_01.py
+++ hw_09_part4_01.py
@@ -3,3 +3,10 @@
 # třídu Blender (obsahuje informace o mixéru), třídu MeatGrinder (obsahuje informace o mlýnku na maso).
 # Každá třída musí mít požadované metody.
 
+class Device:
+    def __init__(self, znacka, model):
+        self.znacka = znacka
+        self.model = model
+
+
+    def
