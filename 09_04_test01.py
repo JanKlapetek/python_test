57be9468c66506a286b9ddda3293d7d42dab4a3d
JanKlapetek <jan.klapetek@gmail.com>
2024-04-09 21:17:34 +0200

    dnešní opakování _pretezovani operatoru, magicke operatory

--- 09_04_test01.py
+++ 09_04_test01.py
@@ -7,9 +7,16 @@ class Book:
     def __str__(self):
         return (f'Title: {self.title} \nAuthor: {self.author}, \nPages: {self.pages}')
 
+    def __add__(self, other):
+        self.other = other
+        return self.other
+
 book1 = Book('Python crash Course', 'Monty Python', 800)
 book2 = Book('JavaScript for begginers', 'Steve Jobs', 820)
+book3 = Book('JavaScript for begginers', 'Steve Jobs', 890)
 print(book1)
-vysledek = book1.author + book2.author
-print(vysledek)
+vysledek = book1.pages + book2.pages + book3.pages
+nazev = book1.title + book2.title + book3.title
+print(f'vysledek je: {vysledek}')
+print(nazev)
 
