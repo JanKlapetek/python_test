ac8795d3530e0a8e32040396948b7feee55973f8
JanKlapetek <jan.klapetek@gmail.com>
2024-04-09 20:47:31 +0200

    dnešní opakování _operatory

---
+++ 09_04_test01.py
@@ -0,0 +1,14 @@
+class Book:
+    def __init__(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = pages
+
+    def show_info(self):
+        print('Title: {}'.format(self.title))
+        print('Author: {}'.format(self.author))
+        print('Pages: {}'.format(self.pages))
+
+book1 = Book('Python crash Course', 'Monty Python', 800)
+book2 = Book('JavaScript for begginers', 'Steve Jobs', 820)
+print(book1+book2)
