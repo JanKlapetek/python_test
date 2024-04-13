72e92eb6bd51985c5f50d4327cf8b17f830354ac
JanKlapetek <jan.klapetek@gmail.com>
2024-04-13 14:10:25 +0200

    hw_09_part3_2.py -- 3. pokus

--- hw_09_part3_2.py
+++ hw_09_part3_2.py
@@ -5,6 +5,7 @@ class TeplotniPrevodnik:
     def celsius_na_f(teplota_c):
         teplota_f = (teplota_c * 1.8) + 32
         return teplota_f
+
     @staticmethod
     def f_na_celsius(teplota_f):
         teplota_c = (teplota_f - 32) / 1.8
@@ -20,15 +21,27 @@ class TeplotniPrevodnik:
             TeplotniPrevodnik.pocet_prepoctu += 1
             return TeplotniPrevodnik.celsius_na_f(teplota)
         elif jednotka.lower() == 'f':
-            TeplotniPrevodnik.f_na_celsius(teplota)
+            TeplotniPrevodnik.pocet_prepoctu += 1
+            return TeplotniPrevodnik.f_na_celsius(teplota)
         else:
-            return 'Neplatná jednotka! Zadej C nebo F! '
+            return 'Neplatná jednotka! Zadej C nebo F!'
 
 teplotni_prevodnik = TeplotniPrevodnik()
-teplota_celsius = 37.5
-teplota_fahrenheit = teplotni_prevodnik.preved(teplota_celsius,'c')
 
-print(f"{teplota_celsius} stupně Celsia se rovná {teplota_fahrenheit:.1f} stupně Fahrenheita.")
-print(f"Počet provedených přepočtů: {teplotni_prevodnik.zjisti_pocet_prepoctu()}")
+while True:
+    try:
+        volba = input("Zadej 'C' pro stupně Celsia nebo 'F' pro stupně Fahrenheita (nebo 'exit' pro ukončení): ")
+        if volba.lower() == 'exit':
+            break
 
+        teplota = float(input("Zadej teplotu: "))
+        prevedena_teplota = teplotni_prevodnik.preved(teplota, volba)
+        if volba.lower() == 'c':
+            print(f"Zadaná teplota se rovná {prevedena_teplota:.1f} stupňů Fahrenheita.")
+        elif volba.lower() == 'f':
+            print(f"Zadaná teplota se rovná {prevedena_teplota:.1f} stupňů Celsia.")
+    except ValueError:
+        print("Neplatný vstup! Zadej číslo jako teplotu.")
+        continue
 
+print(f"Program ukončen. \nPočet provedených přepočtů: {teplotni_prevodnik.zjisti_pocet_prepoctu()}")
