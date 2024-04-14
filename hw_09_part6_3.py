1a0d8d4a3b21f62fc97e1c1a59e240d21e788c62
JanKlapetek <jan.klapetek@gmail.com>
2024-04-14 13:02:08 +0200

    hw_09_part6_3.py class Tvar -- krok9 uprava na pickle!

--- hw_09_part6_3.py
+++ hw_09_part6_3.py
@@ -1,25 +1,21 @@
+import pickle
+
 class Tvar:
     def __init__(self, nazev):
         self.nazev = nazev
+        pass  # Základní inicializace tvaru
+
     def zobraz(self):
-        print(f'Typ tvaru je: {self.nazev}')
+        print('Tvar: ')
+
     def uloz(self, soubor):
-        with open(soubor, 'a') as f:
-            f.write(f'{self.nazev}\n')
-            self._uloz_specificke_informace(f)
+        with open(soubor, 'wb') as soubor:
+            pickle.dump(self, soubor)
 
-    @staticmethod
-    def nacti(soubor):
-        with open(soubor, 'r') as f:
-            nazev = f.readline().strip()
-            tvar = Tvar(nazev)
-            tvar._nacti_specificke_informace(f)
-            return tvar
-
-    def _uloz_specificke_informace(self, f):
-        pass
-    def _nacti_specificke_informace(self, f):
-        pass
+    @classmethod
+    def nacti(cls, soubor):
+        with open(soubor, 'rb') as soubor:
+            return pickle.load(soubor)
 
 class Ctverec(Tvar):
     def __init__(self, nazev, x, y, delka_strany):
@@ -30,17 +26,7 @@ class Ctverec(Tvar):
 
     def zobraz(self):
         super().zobraz()
-        print(f'Souřadnice levého horního rohu: ({self.x}, {self.y})')
-        print(f'Délka strany: {self.delka_strany}')
-
-    def _uloz_specificke_informace(self, f):
-        f.write(f'Souřadnice levého horního rohu: ({self.x}, {self.y})')
-        f.write(f'Délka strany: {self.delka_strany}\n')
-
-    def _nacti_specificke_informace(self, f):
-        x, y = map(int, f.readline().split()[3][1:-1].split(','))
-        self.x, self.y = x, y
-        self.delka_strany = int(f.readline().split()[2])
+        print(f'{self.nazev}: Horní levý roh ({self.x}, {self.y}), Délka strany: {self.delka_strany}\n')
 
 class Obdelnik(Tvar):
     def __init__(self, nazev, x, y, sirka, vyska):
@@ -52,21 +38,9 @@ class Obdelnik(Tvar):
 
     def zobraz(self):
         super().zobraz()
-        print(f'Souřadnice levého horního rohu: {self.x}, {self.y}')
-        print(f'Šířka: {self.sirka}, Výška: {self.vyska}')
+        print(f'{self.nazev}: Levý horní roh ({self.x}, {self.y}), Šířka: {self.sirka}, Výška: {self.vyska}\n')
 
-    def _uloz_specificke_informace(self, f):
-        f.write(f'Souřadnice levého horního rohu: ({self.x}, {self.y})\n')
-        f.write(f'Šířka: {self.sirka}\n')
-        f.write(f'Výška: {self.vyska}\n')
-
-    def _nacti_specificke_informace(self, f):
-        x, y = map(int, f.readline().split()[3][1:-1].split(","))
-        self.x, self.y = x, y
-        self.sirka = int(f.readline().split()[1])
-        self.vyska = int(f.readline().split()[1])
-
-class Kruh(Tvar):
+class Kruznice(Tvar):
     def __init__(self, nazev, stred_x, stred_y, polomer):
         super().__init__(nazev)
         self.stred_x = stred_x
@@ -75,17 +49,7 @@ class Kruh(Tvar):
 
     def zobraz(self):
         super().zobraz()
-        print(f'Souřadnice středu: ({self.stred_x}, {self.stred_y})')
-        print(f'Poloměr: {self.polomer}')
-
-    def _uloz_specificke_informace(self, f):
-        f.write(f'Souřadnice středu: ({self.stred_x}, {self.stred_y})\n')
-        f.write(f'Poloměr: {self.polomer}\n')
-
-    def _nacti_specificke_informace(self, f):
-        x, y = map(int, f.readline().split()[3][1:-1].split(","))
-        self.stred_x, self.stred_y = x, y
-        self.polomer = int(f.readline().split()[1])
+        print(f'{self.nazev}: Střed ({self.stred_x}, {self.stred_y}), Poloměr: {self.polomer}\n')
 
 class Elipsa(Tvar):
     def __init__(self, nazev, x, y, sirka, vyska):
@@ -97,50 +61,25 @@ class Elipsa(Tvar):
 
     def zobraz(self):
         super().zobraz()
-        print(f'Souřadnice levého horního rohu opsaného obdelníku: ({self.x}, {self.y})')
-        print(f'Šířka opsaného obdelníku: {self.sirka}')
-        print(f'Výška opsaného obdelníku: {self.vyska}')
+        print(f'{self.nazev}: Horní levý roh opsaného obdélníku ({self.x}, {self.y}), Šířka: {self.sirka}, Výška: {self.vyska}')
 
-    def _uloz_specificke_informace(self, f):
-        f.write(f'Souřadnice levého horního rohu opsaného obdélníku: ({self.x}, {self.y})\n')
-        f.write(f'Šířka opsaného obdélníku: {self.sirka}\n')
-        f.write(f'Výška opsaného obdélníku: {self.vyska}\n')
+# Příklad použití
+ctverec = Ctverec('Čtverec', 0, 0, 5)
+obdelnik = Obdelnik('Obdélník', 2, 3, 8, 6)
+kruznice = Kruznice('Kružnice', 10, 10, 4)
+elipsa = Elipsa('Elipsa', 5, 5, 12, 8)
 
-    def _nacti_specificke_informace(self, f):
-        x, y = map(int, f.readline().split()[3][1:-1].split(','))
-        self.x, self.y = x, y
-        self.sirka = int(f.readline().split()[1])
-        self.vyska = int(f.readline().split()[1])
+tvary = [ctverec, obdelnik, kruznice, elipsa]
 
-# Vytvoření seznamu všech tvarů
-tvary = [
-    Ctverec('Čtverec', 0, 0, 5),
-    Obdelnik('Obdélník', 0, 0, 8, 6),
-    Kruh('Kruh', 0, 0, 5),
-    Elipsa('Elipsa', 0, 0, 10, 7)
-]
 # Uložení tvarů do souboru
-nazev_souboru = 'tvary_data.txt'
-with open(nazev_souboru, 'w') as f:
-    for tvar in tvary:
-        tvar.uloz(nazev_souboru)
+for i, tvar in enumerate(tvary):
+    tvar.uloz(f'tvar_{i}.pickle')
 
 # Načtení tvarů ze souboru
 nactene_tvary = []
-with open(nazev_souboru, 'r') as f:
-    while True:
-        try:
-            tvar = Tvar.nacti(nazev_souboru)
-            nactene_tvary.append(tvar)
-        except EOFError:
-            break
+for i in range(len(tvary)):
+    nactene_tvary.append(Tvar.nacti(f'tvar_{i}.pickle'))
 
-# Zobrazení informací o každém tvaru
-for tvar in nactene_tvary:
-    tvar.zobraz()
-    print()
-
-
-
-
-
+# Zobrazení informací o načtených tvarech
+for nacteny_tvar in nactene_tvary:
+    nacteny_tvar.zobraz()
