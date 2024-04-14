030b641de4179a15ab2c10d719a23c5ad7501026
JanKlapetek <jan.klapetek@gmail.com>
2024-04-14 14:50:56 +0200

    update 14:40

--- hw_09_part5_03.py
+++ hw_09_part5_03.py
@@ -43,3 +43,23 @@ class Letadlo:
     def __ge__(self, other):
         return self.max_cestujicich >= other.max_cestujicich
 
+letadlo1 = Letadlo(typ=input('Zadej název letadla 1: '), max_cestujicich=input('Zadej max cestujících: '))
+letadlo2 = Letadlo(typ=input('Zadej název letadla 2: '), max_cestujicich=input('Zadej max cestujících: '))
+
+
+print(f"Typ letadla 1: {letadlo1.typ}")
+print(f"Maximální počet cestujících na palubě letadla 1: {letadlo1.max_cestujicich}")
+print(f"Aktuální počet cestujících na palubě letadla 1: {letadlo1.aktualni_cestujici}")
+
+letadlo1 += 200
+print(f"Po přidání 200 cestujících: {letadlo1.aktualni_cestujici}")
+
+if letadlo1 == letadlo2:
+    print("Typy letadel jsou stejné.")
+else:
+    print("Typy letadel nejsou stejné.")
+
+if letadlo1 > letadlo2:
+    print("Letadlo 1 má větší kapacitu než letadlo 2.")
+else:
+    print("Letadlo 1 nemá větší kapacitu než letadlo 2.")
