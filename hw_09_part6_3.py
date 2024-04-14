c791791de460e9eaddf0c81f7dcc18989f85f30f
JanKlapetek <jan.klapetek@gmail.com>
2024-04-14 10:58:45 +0200

    hw_09_part6_3.py class Tvar -- krok2 - ctverec

--- hw_09_part6_3.py
+++ hw_09_part6_3.py
@@ -13,5 +13,18 @@ class Tvar:
         with open(soubor, 'rb') as f:
             return pickle.load(f)
 
+class Ctverec(Tvar):
+    def __init__(self, nazev, x, y, delka_strany):
+        super().__init__(nazev)
+        self.x = x
+        self.y = y
+        self.delka_strany = delka_strany
+
+    def zobraz(self):
+        super().zobraz()
+        print(f'Souřadnice levého horního rohu: ({self.x}, {self.y})')
+        print(f'Délka strany: {self.delka_strany}')
+
+
 
 
