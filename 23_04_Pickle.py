4814efbe27b61113546aed1b23496536d9982caa
JanKlapetek <jan.klapetek@gmail.com>
2024-04-23 20:47:10 +0200

    update final version

---
+++ 23_04_Pickle.py
@@ -0,0 +1,25 @@
+import pickle
+
+def save_data(data, filename):
+    with open(filename, 'wb') as file:
+        return pickle.dump(data, file)
+
+def load_data(filename):
+    with open(filename, 'rb') as file:
+        return pickle.load(file)
+
+def main():
+    user_input = input('Enter lists of integers: ')
+    int_list = [int(item) for item in user_input.split()]
+
+    filename = 'data.pkl'
+
+    save_data(int_list, filename)
+
+    loaded_list = load_data(filename)
+
+    print('Data loaded from file: ', loaded_list)
+
+if __name__ == '__main__':
+    main()
+    
